997475af3e9afade330c6360004708d0e203b008
Y-miu-Y <y.miura.miura.y@gmail.com>
2024-08-20 09:55:35 -0400

    ChakuraUIのボタンを表示して導入されたかを確認する

---
+++ src/StudyRecord.tsx
@@ -0,0 +1,13 @@
+import { Button, ChakraProvider } from "@chakra-ui/react"
+
+
+export const StudyRecord = () => {
+
+  return (
+    <>
+      <ChakraProvider>
+        <Button>おためしボタン</Button>
+      </ChakraProvider>
+    </>
+  )
+}
