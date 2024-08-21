0dc051140cee587a77935f73b6b267d50352c1db
Y-miu-Y <y.miura.miura.y@gmail.com>
2024-08-21 17:13:31 -0400

    ユーザーはデータ取得のときにローディング画面をみることができる(データを取得したらテーブルが表示される) / ユーザーは記録の一覧を見ることができる

--- src/components/pages/RecordsTable.tsx
+++ src/components/pages/RecordsTable.tsx
@@ -1,7 +1,7 @@
 import { FC, memo, useEffect } from "react";
 import { useAllRecords } from "../../hooks/uesAllRecords";
 import { Loading } from "../molecules/Loading";
-import { TableContainer } from "@chakra-ui/react";
+import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import { Header } from "../organism/layout/Header";
 
 export const RecordsTable: FC = memo(() => {
@@ -18,9 +18,28 @@ export const RecordsTable: FC = memo(() => {
       ) : (
         <>
         
-          <Header></Header>
+          <Header />
           <TableContainer>
-            
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>学習内容</Th>
+                  <Th>学習時間</Th>
+                  <Th></Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {records.map((record) => (
+                  <Tr key={record.id}>
+                    <Td>{record.title}</Td>
+                    <Td>{record.time}時間</Td>
+                    <Td>編集</Td>
+                    <Td>削除</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
           </TableContainer>
         </>
       )}
